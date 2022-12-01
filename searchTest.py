40196fb115fd3b186b5e85fbfd89ac41aff604dc
andy-kin <andykin@privatei.com>
2022-11-30 18:33:07 -0700

    first and last commit

---
+++ searchTest.py
@@ -0,0 +1,10 @@
+import json
+
+print ("How much do I love Python")
+
+myDict = {}
+myDict["19crimesItstheend0fthew0rldaswek0owit-spicytac0s"] = "56658A48EE220CC21A1A3CC0443192682F7B3FCF09C3320A5D61E3D80212A703"
+print ("I love it this much >>>>>>>")
+
+for key, val in myDict.items():
+	print ("Key: ", key, " Val: ", val)
